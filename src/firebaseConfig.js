e88000ad623a7abf7d3d4c07d3a5126e0c14a8b3
marioforcelli <32828194+marioforcelli@users.noreply.github.com>
2022-04-18 23:13:56 -0300

    Delete firebaseConfig.js

--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyBhSbYGeZgR_o-pr2BcYewtCmivZeP0WNQ",
-    authDomain: "pollapp-baba6.firebaseapp.com",
-    projectId: "pollapp-baba6",
-    storageBucket: "pollapp-baba6.appspot.com",
-    messagingSenderId: "346848905301",
-    appId: "1:346848905301:web:38d620112dabac6ca06cdc",
-    measurementId: "G-0GLG5QCYRE"
-  }
